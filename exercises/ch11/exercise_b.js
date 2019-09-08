4df6f963c91dfb352e303ddcfc5d74254b4ef09e
EmCeeEs <ab22uhrnachtunruhe@gmail.com>
2019-09-08 13:20:23 +0200

    Add solutions for chapter 11.

--- exercises/ch11/exercise_b.js
+++ exercises/ch11/exercise_b.js
@@ -3,5 +3,9 @@
 //   // eitherToTask :: Either a b -> Task a b
 //   const eitherToTask = either(Task.rejected, Task.of);
 
-// findNameById :: Number -> Task Error (Either Error User)
-const findNameById = compose(map(map(prop('name'))), findUserById);
+// findNameById :: Number -> Task Error User
+const findNameById = compose(
+  map(prop('name')),
+  chain(eitherToTask),
+  findUserById,
+);
